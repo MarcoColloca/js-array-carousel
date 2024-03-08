a10f750d4ac04395319d3a7a3c6950f53c49ee85
MarcoColloca <marco.colloca3511@gmail.com>
2024-03-08 14:49:13 +0100

    Add → added Array and Java Imgs Structure generator

--- js/app.js
+++ js/app.js
@@ -8,3 +8,24 @@ let imagesArray = [
     './img/04.webp',
     './img/05.webp',
 ];
+
+const itemsContainerDOMElement = document.querySelector('.items-container')
+//console.log(itemsContainerDOMElement)
+
+let itemsHTML = '';
+
+for (let i = 0; i < imagesArray.length; i++) {
+
+    const imgSrc = imagesArray[i];
+    //console.log(imgSrc)
+
+    const sliderItemHTML = `
+    <div class="item">
+        <img src="${imgSrc}">
+    </div>
+    `;
+
+    itemsHTML += sliderItemHTML;
+}
+
+itemsContainerDOMElement.innerHTML = itemsHTML
